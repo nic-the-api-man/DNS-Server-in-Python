121563088549eb695c73ee7ab88586f533e6acb1
Nicolas Malixi <nicolas.malixi@gmail.com>
2025-07-13 13:52:29 +0800

    [any message]

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ def main():
     while True:
         try:
             buf, source = udp_socket.recvfrom(512)
-            print(buf, source)
+            print(buf)
             # header = 
             
             response = b"" 
