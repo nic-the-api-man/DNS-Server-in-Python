a2f30c1afe79b3a7e889681d05cd4800920ada92
nic-the-api-man <nicolas.malixi@gmail.com>
2025-07-17 16:43:21 +0800

    [any message]

--- app/main.py
+++ app/main.py
@@ -117,7 +117,7 @@ def main():
             response = b''
             header = DNSHeader()
             question = DNSQuestion('codecrafters.io')
-            answer = DNSAnswer('codecrafters.io', '8.8.8.8')
+            answer = DNSAnswer('8.8.8.8')
             response = header.to_bytes() + question.to_bytes() + answer.to_bytes()
             
     
