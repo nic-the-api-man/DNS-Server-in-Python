635cd89bb601e17815953cf2e6231a72a0a2e48c
Nicolas Malixi <nicolas.malixi@gmail.com>
2025-07-18 18:31:13 +0800

    [any message]

--- app/main.py
+++ app/main.py
@@ -129,7 +129,7 @@ def main():
             header.rd = rd
 
             # Question Parsing
-            print(buf[12])
+            print(buf[12].decode())
             question = DNSQuestion('codecrafters.io')
 
             # Answer Parsing
