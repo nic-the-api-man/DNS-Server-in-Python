23cc3feaf34988e7b50f862950690c6bde5f6ed9
Nicolas Malixi <nicolas.malixi@gmail.com>
2025-07-18 23:56:25 +0800

    [any message]

--- app/main.py
+++ app/main.py
@@ -146,7 +146,7 @@ def main():
             question = DNSQuestion(parsed_domain_name)
 
             # Answer Parsing
-            answer = DNSAnswer('codecrafters.io', '8.8.8.8')
+            answer = DNSAnswer(parsed_domain_name, '8.8.8.8')
 
             response = header.to_bytes() + question.to_bytes() + answer.to_bytes()
             
