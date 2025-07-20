0ea3be1f849fe2068bf0483a2b51bfe20ffa24da
Nicolas Malixi <nicolas.malixi@gmail.com>
2025-07-20 14:35:47 +0800

    [any message]

--- app/main.py
+++ app/main.py
@@ -154,7 +154,7 @@ def main():
             buf, source = udp_socket.recvfrom(512)
             transaction_id = struct.unpack("!H", buf[:2])[0] # Parses transaction ID from buf
             flags = struct.unpack("!H", buf[2:4])[0] # Parses flags from buf, mainly qr, opcode, and rd
-            print(buf)
+            print(source)
             # Flag building          
             qr = (flags >> 15) & 0x1 #1 bit
             opcode = (flags >> 11) & 0xF # 4 bits (bits 11 - 4)
